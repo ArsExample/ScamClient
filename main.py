c38855ddf586d3278a6234d14bc527574ae12aab
Arseniy <arseny.ackimov@yandex.ru>
2021-11-24 00:00:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import socket
+
+sock = socket.socket()
+sock.connect(("localhost", 2345))
+
+commandExit = False
+
+while not commandExit:
+    s = input()
+    if s != "exit":
+        sock.send(s.encode("utf-8"))
+    else:
+        print("Terminating the process")
+        commandExit = True
+        break
+
+    data = str(sock.recv(1024))[2:-1]
+    print(data)
+if commandExit:
+    print("Successfully terminated by client")
+sock.close()
